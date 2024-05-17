331ddbb3e6e1a7e28a96ba9182b90c2d1cdc6c48
Mozoou <m.zouheir74@gmail.com>
2024-05-17 17:10:33 +0300

    added shuffle function

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -4,3 +4,21 @@ export const getFontSize = (size: number) => {
   const fontScale = PixelRatio.getFontScale();
   return size / fontScale;
 }
+
+export const shuffle = (array: Array<any>) => {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
